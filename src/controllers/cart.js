45223ed049e990c510d60c5c7326113ce086cdad
Davi Santos <daviir17@gmail.com>
2025-05-13 04:45:56 -0300

    implemented the functionality to detect categories

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -3,55 +3,65 @@ const Cart = require('../model/cart/cart');
 const CartItem = require('../model/cart/cartItem');
 
 const addItemToCart = async (req, res) => {
-    const { quantity, price, product_id } = req.body;
-    const user_id = req.user.id;
+  const { quantity, price, product_id } = req.body;
+  const user_id = req.user.id;
 
-    const cartItemData = {
-        quantity,
-        price,
-        product_id
+  const cartItemData = {
+    quantity,
+    price,
+    product_id,
+  };
+
+  const transaction = await db.transaction();
+
+  try {
+    // cart
+    let cart = await Cart.findOne({ where: { user_id } }, { transaction });
+
+    if (!cart) cart = await Cart.create({ user_id }, { transaction });
+
+    if (!cart) throw new Error('failed to create cart');
+
+    const cart_id = cart.id;
+
+    // cartItens
+
+    let cartItem = await CartItem.findOne(
+      { where: { product_id, cart_id } },
+      { transaction }
+    );
+
+    if (!cartItem) {
+      cartItem = await CartItem.create(
+        { ...cartItemData, cart_id },
+        { transaction }
+      );
+    } else {
+      const updatedQuantity = cartItem.quantity + quantity;
+      const updatedPrice = updatedQuantity * price;
+      await cartItem.update(
+        { quantity: updatedQuantity, price: updatedPrice },
+        { transaction }
+      );
     }
 
-    const transaction = await db.transaction();
+    if (!cartItem) throw new Error();
 
-    try {
-        // cart
-        let cart = await Cart.findOne({ where: {user_id} }, { transaction });
+    await transaction.commit();
 
-        if(!cart) cart = await Cart.create({ user_id }, { transaction });
+    return res
+      .status(200)
+      .json({ cart: { ...cart.toJSON(), cartItems: cartItem.toJSON() } });
+  } catch (error) {
+    await transaction.rollback();
 
-        if(!cart) throw new Error('failed to create cart');
-
-        const cart_id = cart.id;
-
-        // cartItens
-
-        let cartItem = await CartItem.findOne({ where: {product_id, cart_id } }, { transaction });
-
-        if(!cartItem) {
-            cartItem = await CartItem.create({ ...cartItemData, cart_id }, { transaction });
-
-        } else {
-            const updatedQuantity = cartItem.quantity + quantity;
-            const updatedPrice = updatedQuantity * price;
-            await cartItem.update({ quantity: updatedQuantity, price: updatedPrice }, { transaction });
-        }
-
-        if (!cartItem) throw new Error();
-
-        await transaction.commit();
-
-        return res.status(200).json({ cart: { ...cart.toJSON(), cartItems: cartItem.toJSON()} });
-         
-    } catch (error) {
-        return res.status(400).json({
-            error: "@cart/addItem",
-            message: error.message || "Failed to add item to cart"
-        });
-    }
-    
-}
+    return res.status(400).json({
+      error: '@cart/addItem',
+      message: error.message || 'Failed to add item to cart',
+    });
+  }
+};
 
 module.exports = {
-    addItemToCart
-}
+  addItemToCart,
+};
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,20 +1,37 @@
 const { Router } = require('express');
 const routes = Router();
 
-const { createCategory, getCategories, updateCategories } = require('../controllers/category')
-const { requiSignin, verifyAdmin, verifyUser } = require('../middlewares/verifyAuthentication');
+const {
+  createCategory,
+  getCategories,
+  updateCategories,
+  deleteCategories,
+} = require('../controllers/category');
+
+const {
+  requiSignin,
+  verifyAdmin,
+  verifyUser,
+} = require('../middlewares/verifyAuthentication');
 
 const upload = require('../configs/multer');
-    
-routes.post('/category/create', 
-    requiSignin, 
-    verifyAdmin, 
-    upload.single('categoryImage'), 
-    createCategory
+
+routes.post(
+  '/category/create',
+  requiSignin,
+  verifyAdmin,
+  upload.single('categoryImage'),
+  createCategory
 );
 
 routes.get('/category/getCategories', getCategories);
 
-routes.post('/category/updateCategories', upload.array('categoryImage'), updateCategories);
+routes.post(
+  '/category/updateCategories',
+  upload.array('categoryImage'),
+  updateCategories
+);
 
-module.exports = routes;    
+routes.post('/category/deleteCategories', deleteCategories);
+
+module.exports = routes;
